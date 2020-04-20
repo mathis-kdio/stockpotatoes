b4d5c3b55a9e5cd9c06e41855423394c1b6331a1
mathis kdio <cadio@et.esiea.fr>
2020-04-20 12:48:46 +0200

    Update index.php
    Design avec bootstrap

--- index.php
+++ index.php
@@ -10,37 +10,35 @@ unset($_SESSION['matiere_ID']);
 unset($_SESSION['niveau_ID']);
 unset($_SESSION['theme_ID']);
 unset($_SESSION['nom_mat']);
+
+$titre_page = "Accueil Stockpotatoes";
+$meta_description = "Page principale de Stockpotatoes";
+$meta_keywords = "outils, ressources, exercices en ligne, hotpotatoes";
+$css_deplus = "";
+require('includes/header.inc.php');
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <title>Accueil Stockpotatoes</title>
-    <meta charset="utf-8">
-    <meta http-equiv="Content-Type" content="text/html">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Page principale de Stockpotatoes">
-    <link href="style.css" rel="stylesheet" type="text/css">
-  </head>
-  <body>
-    <header>
-      <div class="center">
-        <img src="patate.gif" width="45" height="41"><img src="stockpotatoes.png" width="324" height="39"> 
-      </div>
-    </header>
-    <section class="">
-      <h3 class="border center">Stockpotatoes: Le distributeur de patates chaudes!</h3>
-      <div class="border center shadow">  
-        <ul class="">
-          <li class="padding-bottom-1"><a href="login_eleve.php">Accès aux documents en mode identifié - Evaluation</a></li>
-          <li class="padding-bottom-1"><a href="accueil_visiteur.php"> Accès aux documents en mode visiteur - Entrainement</a></li>
-          <li class="padding-bottom-1"><a href="upload/login_upload.php">Envoyer un exercice ou un document sur le serveur</a></li>
-          <li class="padding-bottom-1"><a href="administrateur/login_administrateur.php">Administrateur</a></li>
-          <li class="padding-bottom-1"><a href="enseignant/login_enseignant.php">Enseignant</a></li>
-        </ul>
-      </div>
-      <div class="text-center">
-        <h4>Ver 3.0 ALPHA 5 - 17/04/2020</h4>
-      </div>
-    </section>
-  </body>
-</html>
+<div class="row mt-3">
+  <div class="col bg-warning text-center shadow">
+    <h3>Stockpotatoes: Le distributeur de patates chaudes!</h3>
+  </div>
+</div>
+<div class="row mt-3 mb-3 justify-content-center">
+  <div class="col-8 pt-2 pb-2 bg-warning shadow">
+    <ul class="">
+      <li class=""><a href="login_eleve.php">Accès aux documents en mode identifié - Evaluation</a></li>
+      <li class=""><a href="accueil_visiteur.php"> Accès aux documents en mode visiteur - Entrainement</a></li>
+      <li class=""><a href="upload/login_upload.php">Envoyer un exercice ou un document sur le serveur</a></li>
+      <li class=""><a href="administrateur/login_administrateur.php">Administrateur</a></li>
+      <li class=""><a href="enseignant/login_enseignant.php">Enseignant</a></li>
+    </ul>
+  </div>
+</div>
+<div class="row">
+  <div class="col bg-warning text-center shadow">
+    <h4>Ver 3.0 ALPHA 5 - 17/04/2020</h4>
+  </div>  
+</div>
+</section>
+<?php
+require('includes/footer.inc.php');
+?>

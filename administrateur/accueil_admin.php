a9adf6feddc2c2455c4dd0bb060cce35906c2125
mathis kdio <cadio@et.esiea.fr>
2020-04-24 19:12:01 +0200

    Update accueil_admin.php
    Nouveau design avec Bootstrap semblable à accueil_enseignant

--- administrateur/accueil_admin.php
+++ administrateur/accueil_admin.php
@@ -1,82 +1,72 @@
 <?php session_start();
-if (isset($_SESSION['Sess_nom'])) { 
-	if ($_SESSION['Sess_nom']<>'Administrateur') { header("Location: login_administrateur.php");}
-; } else { header("Location: ../index.php");}?>
-<html>
-<head>
-<title>Espace Administrateur</title>
-<meta charset="utf-8">
-<meta http-equiv="Content-Type" content="text/html">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link href="../style_jaune.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-<p> <a href="../index.php"><img src="../patate.gif" width="53" height="45" border="0"></a> 
-  <img src="../patate.jpg" width="324" height="39" align="top"> </p>
-<p><strong> <a href="../index.php">Accueil Stockpotatoes</a> - Espace Administrateur</strong></p>
-<p>&nbsp;</p>
-<table width="100%" border="0" cellspacing="10" cellpadding="0">
-  <tr valign="middle" bgcolor="#CCCC99"> 
-    <td> <blockquote> 
-        <p><a href="gestion_matiere_niveau.php"><strong>Gestion des mati&egrave;res 
-          et niveaux</strong></a></p>
-      </blockquote></td>
-    <td> <blockquote> 
-        <p><a href="http://localhost/mysql/"><strong>Acc&egrave;s en intranet 
-          &agrave; la base via PhpMyAdmin</strong></a> - ( <a href="aide_phpmyadmin.php">Aide</a>)</p>
-      </blockquote></td>
-  </tr>
-  <tr valign="middle" bgcolor="#CCCC99"> 
-    <td> <blockquote> 
-        <p><strong><a href="gestion_eleve.php">Gestion des &eacute;l&egrave;ves</a></strong></p>
-      </blockquote></td>
-    <td> <blockquote> 
-        <p><strong><a href="install_param3.php">Gestion des mots de passe</a></strong></p>
-      </blockquote></td>
-  </tr>
-  <tr valign="middle" bgcolor="#CCCC99"> 
-    <td><blockquote> 
-        <p><a href="gestion_activite.php"><strong>Gestion des activit&eacute;s</strong></a></p>
-      </blockquote></td>
-    <td><blockquote> 
-        <p><a href="../enseignant/config.php"><strong>Param&eacute;trage du Hotpotatoes 
-          des enseignants </strong></a></p>
-      </blockquote></td>
-  </tr>
-  <tr valign="middle" bgcolor="#CCCC99"> 
-    <td><blockquote> 
-        <p><strong><a href="../enseignant/liste_activite.php">Les derni&egrave;res 
-          activit&eacute;s</a></strong></p>
-      </blockquote></td>
-    <td><blockquote> 
-        <p><a href="install_param.php"><strong>Param&eacute;trage de la connexion 
-          &agrave; la base de donn&eacute;es</strong></a></p>
-      </blockquote></td>
-  </tr>
-  <tr valign="middle" bgcolor="#CCCC99">
-    <td><blockquote>
-      <p><a href="../enseignant/accueil_enseignant.php"><strong>Espace Enseignant</strong></a> - <a href="../index.php"><strong>Espace El&egrave;ve</strong></a> - <strong> <a href="../documentation.htm" target="_blank">Documentation </a></strong></p>
-    </blockquote></td>
-    <td><blockquote>
-      <p><a href="../install/install_editeur.php"><strong>Param&eacute;trage de l'&eacute;diteur en ligne </strong></a></p>
-      <p><a href="aide_editeur.php"><strong>Aide sur le fonctionnement de l'&eacute;diteur</strong></a><br>
-      </p>
-      </blockquote></td>
-  </tr>
-  <tr valign="middle" bgcolor="#CCCC99"> 
-    <td><blockquote> 
-        <p><strong><a href="../upload/upload_menu.php">Envoyer un exercice ou un document</a></strong></p>
-      </blockquote></td>
-    <td><blockquote> 
-        <p><a href="sauvegarde.php"><strong>Sauvegarde de Stockpotatoes</strong></a> </p>
-      </blockquote></td>
-  </tr>
-</table>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p align="center">&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp; </p>
-</body>
-</html>
+if (isset($_SESSION['Sess_nom']))
+{ 
+	if ($_SESSION['Sess_nom']<>'Administrateur')
+	{
+		header("Location: login_administrateur.php");
+	}
+}
+else
+{ 
+	header("Location: ../index.php");
+}
+
+$titre_page = "Espace Administrateur";
+$meta_description = "Page d'accueil pour les administrateurs";
+$meta_keywords = "outils, ressources, exercices en ligne, hotpotatoes";
+$js_deplus="";
+$css_deplus = "";
+
+require('include/headerAdministrateur.inc.php');
+?>
+<div class="row">
+	<div class="col-12">
+		<div class="row">
+			<h1>Espace Administrateur</h1>
+		</div>
+		<div class="row">
+			<div class="col-3">
+				<img class="img-fluid rounded mx-auto d-block" src="../patate.png" alt="hotpotatoes" title="hotpotatoes" height="150" width="150" />
+			</div>
+			<div class="col-9 align-middle">
+				<p class="h3 bg-warning text-center p-3" style="margin-top: 50px;">Accueil de l'espace Administrateur</p>
+			</div>
+		</div>
+		<div class="container jumbotron">
+			<div class="row mb-5">
+				<div class="col-6">
+					<h3 class="bg-warning shadow text-center pb-2 mb-4">Gestion</h3>
+					<div class="list-group">
+						<a href="gestion_matiere_niveau.php" class="list-group-item list-group-item-action list-group-item-primary">Gestion des matières et niveaux</a>
+						<a href="gestion_eleve.php" class="list-group-item list-group-item-action list-group-item-primary">Gestion des élèves</a>
+						<a href="install_param3.php" class="list-group-item list-group-item-action list-group-item-primary">Gestion des mots de passe</a>
+						<a href="gestion_activite.php" class="list-group-item list-group-item-action list-group-item-primary">Gestion des activités</a>
+					</div>
+				</div>
+				<div class="col-6">
+					<h3 class="bg-warning shadow text-center pb-2 mb-4">Paramétrage</h3>
+					<div class="list-group">
+						<a href="../enseignant/config.php" class="list-group-item list-group-item-action list-group-item-primary">Paramétrage du Hotpotatoes des enseignants</a>
+						<a href="install_param.php" class="list-group-item list-group-item-action list-group-item-primary">Paramétrage de la connexion à la base de données</a>
+						<a href="../install/install_editeur.php" class="list-group-item list-group-item-action list-group-item-primary">Paramétrage de l'éditeur en ligne</a>
+						<a href="sauvegarde.php" class="list-group-item list-group-item-action list-group-item-primary">Sauvegarde de Stockpotatoes</a>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12">
+					<h3 class="bg-warning shadow text-center pb-2 mb-4">Documentations</h3>
+					<div class="list-group">
+						<a href="../documentation.htm" target="_blank" class="list-group-item list-group-item-action list-group-item-primary">Documentation complète</a>
+						<a href="aide_editeur.php" class="list-group-item list-group-item-action list-group-item-primary">Aide sur le fonctionnement de l'éditeur</a>
+						<a href="http://www.ac-orleans-tours.fr/ses/pedagogie/utilisation%20tice/exo_hotpot/sommaire.htm" class="list-group-item list-group-item-action list-group-item-primary">Tutoriel Hotpotatoes 6</a>
+						<a href="aide_phpmyadmin.php" class="list-group-item list-group-item-action list-group-item-primary">Aide accès en intranet à la base via PhpMyAdmin</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+require('include/footerAdministrateur.inc.php');
+?>

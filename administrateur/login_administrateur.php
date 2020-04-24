75133b3497f50062ef18d48ac060ba33f3c160ef
mathis kdio <cadio@et.esiea.fr>
2020-04-24 18:52:31 +0200

    Bootstrap pour espace admin

--- administrateur/login_administrateur.php
+++ administrateur/login_administrateur.php
@@ -1,51 +1,64 @@
-<?php 
+<?php
+include("../Connections/gestion_pass.inc.php");
 if (isset($_POST['pass_administrateur'])) 
 {
-include("../Connections/gestion_pass.inc.php");
-if ($_POST['pass_administrateur']==$pass_admin)
-{
-session_start();
-$_SESSION['Sess_nom'] ='Administrateur';
-header("Location: accueil_admin.php");
+	if (htmlspecialchars($_POST['pass_administrateur']) == $pass_admin)
+	{
+		session_start();
+		$_SESSION['Sess_nom'] = 'Administrateur';
+		header("Location: accueil_admin.php");
+	}
+	else
+	{
+		$bad_password = 1;
+	}
 }
-}
-?> 
-<html>
-<head>
-<title>Login Administrateur</title>
-<meta charset="utf-8">
-<meta http-equiv="Content-Type" content="text/html">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link href="../style_jaune.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-<p>&nbsp;</p>
-<table width="100%" border="0" cellspacing="10" cellpadding="0">
-  <tr valign="top"> 
-    <td width="38%"><img src="../patate.gif" width="253" height="227"></td>
-    <td width="62%"> <p><img src="../patate.jpg" width="324" height="39" align="top"></p>
-      <p><strong>Espace administrateur</strong></p>
-      <form name="form1" method="post" action="login_administrateur.php">
-        <p>&nbsp;</p>
-        <p align="center">Entrer votre mot de passe 
-          <input name="pass_administrateur" type="password" id="pass_administrateur">
-          <input type="submit" name="Submit" value="Valider">
-        </p>
-        <p>&nbsp; </p>
-      </form>
-	  <SCRIPT>document.form1.pass_administrateur.focus();</SCRIPT>
-      <p>&nbsp;</p>
-      <p>&nbsp;</p></td>
-  </tr>
-</table>
-<p>&nbsp;</p>
-<p align="center"><a href="../index.php">Accueil Stockpotatoes</a> - <a href="../enseignant/login_enseignant.php">Espace 
-  Enseignant</a> </p>
-<p align="center"><a href="../upload/login_upload.php">Envoyer un exercice ou un document 
-  sur le serveur</a></p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp; </p>
-</body>
-</html>
+
+$titre_page = "Login Administrateur";
+$meta_description = "Page de login pour les administrateurs";
+$meta_keywords = "outils, ressources, exercices en ligne, hotpotatoes";
+$js_deplus="";
+$css_deplus = "";
+
+require('include/headerAdministrateur.inc.php');
+?>
+<div class="row">
+	<div class="col-12">
+		<div class="row">
+			<h1>Espace Administrateur</h1>
+		</div>
+		<div class="row">
+			<div class="col-3">
+				<img class="img-fluid rounded mx-auto d-block" src="../patate.png" alt="hotpotatoes" title="hotpotatoes" height="150" width="150" />
+			</div>
+			<div class="col-9 align-middle">
+				<p class="h3 bg-warning text-center p-3" style="margin-top: 50px;">Connection à l'espace Administrateur</p>
+			</div>
+		</div>
+		<div class="row jumbotron">
+			<div class="col">
+				<?php 
+				if ($bad_password == 1)
+				{
+					echo '<h4 class="text-center" style="color:red">MOT DE PASSE INCORRECT</h4>';
+				} ?>
+				<form name="form1" method="post" action="login_administrateur.php">
+					<div class="form-group form-row justify-content-center">
+						<div class="col-4">
+							<label for="pass_administrateur">Entrer votre mot de passe</label>
+							<input type="password" class="form-control" name="pass_administrateur" id="pass_administrateur" placeholder="Mot de passe">
+						</div>
+					</div>
+					<div class="form-group form-row justify-content-center">
+						<div class="col-4">
+							<button type="submit" name="Submit" class="btn btn-primary">Valider</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+require('include/footerAdministrateur.inc.php');
+?>

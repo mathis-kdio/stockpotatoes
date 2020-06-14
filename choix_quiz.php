ed8dba1023deb4758372afeed78dd0e14b78d998
mathis kdio <cadio@et.esiea.fr>
2020-06-14 19:49:27 +0200

    Update choix_quiz.php
    Nettoyage et sécurité de base

--- choix_quiz.php
+++ choix_quiz.php
@@ -1,29 +1,39 @@
 <?php 
 session_start();
+
 function sans_accent($chaine) 
-{ 
-   $accent  ="ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿ"; 
-   $noaccent="aaaaaaaceeeeiiiidnoooooouuuuybsaaaaaaaceeeeiiiidnoooooouuuyyby"; 
-   return strtr(trim($chaine),$accent,$noaccent); 
-} 	
-	
+{
+	 $accent  ="ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿ";
+	 $noaccent="aaaaaaaceeeeiiiidnoooooouuuuybsaaaaaaaceeeeiiiidnoooooouuuyyby";
+	 return strtr(trim($chaine), $accent, $noaccent); 
+}
 
-    $_SESSION['Sess_ID_quiz'] = $_GET['VAR_ID_quiz'];
-	$fichier=$_GET['VAR_fichier'];
-	$nom_mat=sans_accent($_GET['VAR_nom_mat']);
+$_SESSION['Sess_ID_quiz'] = htmlspecialchars($_GET['VAR_ID_quiz']);
+$fichier = htmlspecialchars($_GET['VAR_fichier']);
+$nom_mat = sans_accent(htmlspecialchars($_GET['VAR_nom_mat']));
 
-	$_SESSION['nom_mat'] = $nom_mat;
-	
-	if (isset($_GET['matiere_ID'])){$_SESSION['matiere_ID'] = $_GET['matiere_ID'];}
-	if (isset($_GET['niveau_ID'])){$_SESSION['niveau_ID'] = $_GET['niveau_ID'];}
-	if (isset($_GET['theme_ID'])){$_SESSION['theme_ID'] = $_GET['theme_ID'];}
-	if (isset($_GET['categorie_ID'])){$_SESSION['categorie_ID'] = $_GET['categorie_ID'];}
-	
-	
-	$lien="Exercices/".$nom_mat."/q".$_GET['VAR_ID_quiz']."/".$fichier;
-	
+$_SESSION['nom_mat'] = $nom_mat;
 
-	
-	header("Location: $lien");
-	
+if (isset($_GET['matiere_ID']))
+{
+	$_SESSION['matiere_ID'] = htmlspecialchars($_GET['matiere_ID']);
+}
+
+if (isset($_GET['niveau_ID']))
+{
+	$_SESSION['niveau_ID'] = htmlspecialchars($_GET['niveau_ID']);
+}
+
+if (isset($_GET['theme_ID']))
+{$_SESSION['theme_ID'] = htmlspecialchars($_GET['theme_ID']);
+}
+
+if (isset($_GET['categorie_ID']))
+{
+	$_SESSION['categorie_ID'] = htmlspecialchars($_GET['categorie_ID']);
+}
+
+$lien = "Exercices/".$nom_mat."/q".htmlspecialchars($_GET['VAR_ID_quiz'])."/".$fichier;
+
+header("Location: $lien");
 ?>

ab3ed2b0d110ca1c3c3eff06dde2bb96e5a61110
mathis kdio <cadio@et.esiea.fr>
2020-06-14 16:24:08 +0200

    Update index.php
    meilleur redirection

--- upload/index.php
+++ upload/index.php
@@ -1,4 +1,12 @@
 <?php session_start();
-if (isset($_SESSION['Sess_nom'])) { 
-	if ($_SESSION['Sess_nom']<>'Upload') { header("Location: login_upload.php");}
-; } else { header("Location: ../index.php");}?>
+if (isset($_SESSION['Sess_nom']))
+{ 
+	if ($_SESSION['Sess_nom'] <> 'Upload')
+		header("Location: login_upload.php");
+	else
+		header("Location: upload_menu.php")
+}
+else
+{
+	header("Location: login_upload.php");
+}?>

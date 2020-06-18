506836674328132331c3f64a026deb2f739b52c9
mathis kdio <cadio@et.esiea.fr>
2020-06-18 22:06:57 +0200

    wip

--- administrateur/gestion_theme.php
+++ administrateur/gestion_theme.php
@@ -21,23 +21,25 @@ $meta_keywords = "outils, ressources, exercices en ligne, hotpotatoes";
 $js_deplus = "";
 $css_deplus = "";
 
+require_once('../includes/yml.class.php');
+
 require('include/headerAdministrateur.inc.php');
 ?>
 
-			<form method="POST" action="?&action=themeColor" class="row">
-				<div class="col-sm-6">
-					<h5>Theme</h5>
-					<?php $lecture = new Lire('../include/config.yml');
-					$lecture = $lecture->GetTableau()?>
-					<label>Couleur principale</label><br>
-					<input type="color" name='color_theme_main'<?php if(isset($lecture["color"]["theme"]["main"])) echo ' value="'.$lecture["color"]["theme"]["main"].'"'?>><br><br>
-					<label>Couleur lors du survol</label><br>
-					<input type="color" name='color_theme_hover'<?php if(isset($lecture["color"]["theme"]["hover"])) echo ' value="'.$lecture["color"]["theme"]["hover"].'"'?>><br><br>
-					<label>Couleur lors du clic</label><br>
-					<input type="color" name='color_theme_focus'<?php if(isset($lecture["color"]["theme"]["focus"])) echo ' value="'.$lecture["color"]["theme"]["focus"].'"'?>><br><br>
-				</div>
-				<input type="submit" class="btn btn-success" value="Valider les changements">
-			</form>
+	<form method="POST" action="" class="row">
+		<div class="col-sm-6">
+			<h5>Theme</h5>
+			<?php $lecture = new Lire('../includes/config.yml');
+			$lecture = $lecture->GetTableau()?>
+			<label>Couleur principale</label><br>
+			<input type="color" name='color_theme_main'<?php if(isset($lecture["color"]["theme"]["main"])) echo ' value="'.$lecture["color"]["theme"]["main"].'"'?>><br><br>
+			<label>Couleur lors du survol</label><br>
+			<input type="color" name='color_theme_hover'<?php if(isset($lecture["color"]["theme"]["hover"])) echo ' value="'.$lecture["color"]["theme"]["hover"].'"'?>><br><br>
+			<label>Couleur lors du clic</label><br>
+			<input type="color" name='color_theme_focus'<?php if(isset($lecture["color"]["theme"]["focus"])) echo ' value="'.$lecture["color"]["theme"]["focus"].'"'?>><br><br>
+			<input type="submit" class="" value="Valider les changements">
+		</div>
+	</form>
 
 
 <?php

9267aa3f4f675e37a5dbe2a63c2103cdb68635b8
mathis kdio <cadio@et.esiea.fr>
2020-07-01 22:12:17 +0200

    Update liste_quiz_recent.php

--- enseignant/liste_quiz_recent.php
+++ enseignant/liste_quiz_recent.php
@@ -1,100 +1,81 @@
-<?php 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-
-session_start(); 
-if (isset($_SESSION['Sess_nom'])) { 
-	if ($_SESSION['Sess_nom']<>'Enseignant') { header("Location: login_enseignant.php");}
-; } else { header("Location: ../index.php");}?>
-<?php 
-require_once('../Connections/conn_intranet.php'); 
-
-
-$maxRows_rsListeSelectMatiereNiveau = 10;
-$pageNum_rsListeSelectMatiereNiveau = 0;
-if (isset($_GET['pageNum_rsListeSelectMatiereNiveau'])) {
-  $pageNum_rsListeSelectMatiereNiveau = $_GET['pageNum_rsListeSelectMatiereNiveau'];
+<?php
+session_start();
+if (isset($_SESSION['Sess_nom']))
+{
+	if ($_SESSION['Sess_nom'] <> 'Enseignant')
+	{
+		header("Location: login_enseignant.php?cible=liste_quiz_recent");
+	}
 }
-$startRow_rsListeSelectMatiereNiveau = $pageNum_rsListeSelectMatiereNiveau * $maxRows_rsListeSelectMatiereNiveau;
+else
+{
+	header("Location: login_enseignant.php?cible=liste_quiz_recent");
+}
 
+require_once('../Connections/conn_intranet.php');
 mysqli_select_db($conn_intranet, $database_conn_intranet);
-$query_rsListeSelectMatiereNiveau = "SELECT * FROM stock_quiz,stock_matiere,stock_niveau WHERE stock_quiz.matiere_ID=stock_matiere.ID_mat  AND stock_quiz.niveau_ID=stock_niveau.ID_niveau ORDER BY stock_quiz.ID_quiz  DESC";
-$query_limit_rsListeSelectMatiereNiveau = sprintf("%s LIMIT %d, %d", $query_rsListeSelectMatiereNiveau, $startRow_rsListeSelectMatiereNiveau, $maxRows_rsListeSelectMatiereNiveau);
-$rsListeSelectMatiereNiveau = mysqli_query($conn_intranet, $query_limit_rsListeSelectMatiereNiveau) or die(mysqli_error());
-$row_rsListeSelectMatiereNiveau = mysqli_fetch_assoc($rsListeSelectMatiereNiveau);
 
-if (isset($_GET['totalRows_rsListeSelectMatiereNiveau'])) {
-  $totalRows_rsListeSelectMatiereNiveau = $_GET['totalRows_rsListeSelectMatiereNiveau'];
-} else {
-  $all_rsListeSelectMatiereNiveau = mysqli_query($conn_intranet, $query_rsListeSelectMatiereNiveau);
-  $totalRows_rsListeSelectMatiereNiveau = mysqli_num_rows($all_rsListeSelectMatiereNiveau);
+if (isset($_POST['matiere_ID'])) {
+	$matiereId = htmlspecialchars($_POST['matiere_ID']);
 }
-$totalPages_rsListeSelectMatiereNiveau = ceil($totalRows_rsListeSelectMatiereNiveau/$maxRows_rsListeSelectMatiereNiveau)-1;
-
+if (isset($_POST['niveau_ID'])) {
+	$niveauId = htmlspecialchars($_POST['niveau_ID']);
+}
 
 function sans_accent($chaine) 
 { 
-   $accent  ="ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿ"; 
-   $noaccent="aaaaaaaceeeeiiiidnoooooouuuuybsaaaaaaaceeeeiiiidnoooooouuuyyby"; 
-   return strtr(trim($chaine),$accent,$noaccent); 
-} 
+	 $accent  ="ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿ"; 
+	 $noaccent="aaaaaaaceeeeiiiidnoooooouuuuybsaaaaaaaceeeeiiiidnoooooouuuyyby"; 
+	 return strtr(trim($chaine), $accent,$noaccent); 
+}
+
+$query_limit_rsListeSelectMatiereNiveau = "SELECT * FROM stock_quiz, stock_matiere, stock_niveau WHERE stock_quiz.matiere_ID = stock_matiere.ID_mat AND stock_quiz.niveau_ID = stock_niveau.ID_niveau ORDER BY stock_quiz.ID_quiz  DESC LIMIT 0, 10";
+
+$rsListeSelectMatiereNiveau = mysqli_query($conn_intranet, $query_limit_rsListeSelectMatiereNiveau) or die(mysqli_error($conn_intranet));
+
+$titre_page = "Liste des exercices mis en ligne récemment";
+$meta_description = "Page de la liste des exercices mis en ligne récemment";
+$meta_keywords = "outils, ressources, exercices en ligne, hotpotatoes";
+$js_deplus = "";
+$css_deplus = "";
+require('includes/headerEnseignant.inc.php');
 ?>
-<html>
-<head>
-<title>Exercices r&eacute;cemment publi&eacute;s</title>
-<meta charset="utf-8">
-<meta http-equiv="Content-Type" content="text/html">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link href="../style_jaune.css" rel="stylesheet" type="text/css">
 
-</head>
+<div class="table-responsive">
+	<table class="table table-striped table-bordered table-sm">
+		<thead>
+			<tr> 
+				<th scope="col">N° Ex.</th>
+				<th scope="col">Titre</th>
+				<th scope="col">Fichier</th>
+				<th scope="col">Auteur</th>
+				<th scope="col">En entraînement</th>
+				<th scope="col">En évaluation</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php 
+			while ($row_rsListeSelectMatiereNiveau = mysqli_fetch_assoc($rsListeSelectMatiereNiveau))
+			{ 
+  			$matiere = sans_accent($row_rsListeSelectMatiereNiveau['nom_mat']); ?>
+				<tr> 
+					<td><?php echo $row_rsListeSelectMatiereNiveau['ID_quiz']; ?></td>
+					<td>
+						<a href="<?php echo '../Exercices/'.$matiere.'/q'.$row_rsListeSelectMatiereNiveau['ID_quiz']; ?>/<?php echo $row_rsListeSelectMatiereNiveau['fichier']; ?>"><?php echo $row_rsListeSelectMatiereNiveau['titre']; ?></a>
+					</td>
+					<td><?php echo $row_rsListeSelectMatiereNiveau['fichier']; ?></td>
+					<td><?php echo $row_rsListeSelectMatiereNiveau['auteur']; ?></td>
+					<td><?php echo $row_rsListeSelectMatiereNiveau['en_ligne']; ?></td>
+					<td><?php echo $row_rsListeSelectMatiereNiveau['avec_score']; ?></td>
+				</tr>
+				<?php
+			}?>
+		</tbody>
+	</table>
+</div>
 
-<body>
-<p><a href="../index.php"><img src="../patate.gif" width="51" height="42" border="0"></a> 
-  <img src="../patate.jpg" width="324" height="39" align="top"></p>
-<p><strong><a href="../index.php">Accueil Stockpotatoes</a> - </strong><strong><a href="accueil_enseignant.php">Espace Enseignant</a> - Exercices 
-    r&eacute;cemment publi&eacute;s en ligne</strong></p>
-<table border="1">
-  <tr bgcolor="#CCCC99"> 
-    <td> <div align="center"><strong>N&deg; Ex.</strong></div></td>
-    <td> <div align="center"><strong>Titre de l'exercice</strong></div></td>
-    <td> <div align="center"><strong>Fichier</strong></div></td>
-    <td> <div align="center"><strong>Mati&egrave;re</strong></div></td>
-    <td> <div align="center"><strong>Niveau</strong></div></td>
-    <td> <div align="center"><strong>Auteur</strong></div></td>
-    <td> <div align="center"><strong>En entra&icirc;nement</strong></div></td>
-    <td> <div align="center"><strong>En &eacute;valuation</strong></div></td>
-  </tr>
-  <?php do 
-  { 
-  $matiere=sans_accent($row_rsListeSelectMatiereNiveau['nom_mat']);
-  ?>
-  <tr class="retrait20"> 
-    <td><div align="right"><?php echo $row_rsListeSelectMatiereNiveau['ID_quiz']; ?></div></td>
-    <td class="retrait20"><a href="<?php echo '../Exercices/'.$matiere.'/q'.$row_rsListeSelectMatiereNiveau['ID_quiz']; ?>/<?php echo $row_rsListeSelectMatiereNiveau['fichier']; ?>"><strong><?php echo $row_rsListeSelectMatiereNiveau['titre']; ?></strong></a></td>
-    <td class="retrait20"><?php echo $row_rsListeSelectMatiereNiveau['fichier']; ?></td>
-    <td class="retrait20"><?php echo $row_rsListeSelectMatiereNiveau['nom_mat']; ?></td>
-    <td class="retrait20"><?php echo $row_rsListeSelectMatiereNiveau['nom_niveau']; ?></td>
-    <td class="retrait20"><?php echo $row_rsListeSelectMatiereNiveau['auteur']; ?></td>
-    <td><div align="center"><?php echo $row_rsListeSelectMatiereNiveau['en_ligne']; ?></div></td>
-    <td><div align="center"><?php echo $row_rsListeSelectMatiereNiveau['avec_score']; ?></div></td>
-  </tr>
-  <?php } while ($row_rsListeSelectMatiereNiveau = mysqli_fetch_assoc($rsListeSelectMatiereNiveau)); ?>
-</table>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p align="center"><a href="../index.php">Accueil Stockpotatoes</a> - <a href="../enseignant/accueil_enseignant.php">Espace 
-  Enseignant</a> - <a href="../administrateur/login_administrateur.php">Espace 
-  Administrateur</a></p>
-<p align="center"><a href="../upload/upload_menu.php">Envoyer un exercice ou un document 
-  sur le serveur</a></p>
-</html>
 <?php
+require('includes/footerEnseignant.inc.php');
+
 mysqli_free_result($rsListeSelectMatiereNiveau);
 ?>
-
-</body>
-
-
-

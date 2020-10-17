b719033a7d9413c3ed44806ca5786f56aa19448c
mathis kdio <cadio@et.esiea.fr>
2020-10-17 22:55:32 +0200

    Début utilisation sortableJS pour paramétrer navBar espace élève

--- administrateur/gestion_espace_eleve.php
+++ administrateur/gestion_espace_eleve.php
@@ -34,7 +34,7 @@ $lecture = $lecture->GetTableau();
 $titre_page = "Gestion de l'espace élève";
 $meta_description = "Page de gestion de l'espace élève";
 $meta_keywords = "outils, ressources, exercices en ligne, hotpotatoes";
-$js_deplus = "";
+$js_deplus = "include/Sortable.js";
 $css_deplus = "";
 
 require('include/headerAdministrateur.inc.php');
@@ -57,6 +57,53 @@ require('include/headerAdministrateur.inc.php');
 				<button type="submit" name="demandePassword" value="1" class="btn btn-primary">Valider les changements</button>
 			</div>
 		</form>
+	</div>
+</div>
+<h5 class="text-center">Personnaliser la barre de navigation de l'espace élève:</h5>
+<div class="row justify-content-center">
+	<div class="col-auto">
+		<div id="sortablelist" class="list-group list-group-horizontal mb-4" data-id="1">
+			<?php 
+				do { ?>
+					<div class="list-group-item d-flex align-items-center justify-content-between text-center overflow-auto" data-id="<?php echo $row_RsTheme['ID_theme']?>">
+						<div class="col-1" style="cursor: grab;">
+							<img class="position-handle" src="images/move.png" width="19" height="19">
+						</div>
+						<div class="col-1">
+							<?php echo $row_RsTheme['ID_theme']; ?>
+						</div>
+						<div class="col-2">
+							<?php echo $row_RsTheme['theme'];?>
+						</div>
+					</div>
+					<?php 
+				} while ($row_RsTheme = mysqli_fetch_assoc($RsTheme)); ?>
+		</div>
+		<div class="form-group row align-items-center justify-content-center">	
+			<form method="post" name="form_nouvel_ordre" action="gestion_espace_eleve.php">
+				<div class="col-auto">
+					<button type="submit" name="submit_nouvel_ordre" class="btn btn-primary" onclick="setValuesInputOrderList()">Enregistrer le nouvel ordre</button>
+				</div>
+				<input type="hidden" name="MM_nouvel_ordre" value="form_nouvel_ordre">
+				<input type="hidden" id="ordreItemsMenu" name="ordreItemsMenu" value="">
+			</form>
+		</div>
+		<script type="text/javascript">
+			var list = Sortable.create(sortablelist, {
+				animation: 100,
+				group: 'list-1',
+				draggable: '.list-group-item',
+				handle: '.position-handle',
+				sort: true,
+				filter: '.sortable-disabled',
+				chosenClass: 'active',
+			});
+			function setValuesInputOrderList() {
+				var order = list.toArray();
+				let inputOrdreItemsMenu = document.getElementById('ordreItemsMenu');
+				inputOrdreItemsMenu.setAttribute('value', order);
+			}
+		</script>
 	</div>
 </div>
 <?php

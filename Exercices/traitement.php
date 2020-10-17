67b5f3c21186599c707490b7e0f0b64f5ab7d383
mathis kdio <cadio@et.esiea.fr>
2020-10-17 22:52:49 +0200

    Si l'élève refait un exercice mais fait un moins bon score, la meilleure note est gardée.

--- Exercices/traitement.php
+++ Exercices/traitement.php
@@ -1,13 +1,16 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 session_start();
+
 require_once('../Connections/conn_intranet.php');
 $colname_rsActivite = "1";
 if (isset($_SESSION['Sess_ID_eleve'])) {
-  $colname_rsActivite = (get_magic_quotes_gpc()) ? $_SESSION['Sess_ID_eleve'] : addslashes($_SESSION['Sess_ID_eleve']);
+	$colname_rsActivite = (get_magic_quotes_gpc()) ? $_SESSION['Sess_ID_eleve'] : addslashes($_SESSION['Sess_ID_eleve']);
 }
 $colname2_rsActivite = "1";
 if (isset($_SESSION['Sess_ID_quiz'])) {
-  $colname2_rsActivite = (get_magic_quotes_gpc()) ? $_SESSION['Sess_ID_quiz'] : addslashes($_SESSION['Sess_ID_quiz']);
+	$colname2_rsActivite = (get_magic_quotes_gpc()) ? $_SESSION['Sess_ID_quiz'] : addslashes($_SESSION['Sess_ID_quiz']);
 }
 if ($_POST["Start_Time"] == "") $debut = "inconnu";
 	else $debut = $_POST["Start_Time"];
@@ -19,7 +22,7 @@ if ($_POST["realname"] == "") $eleve = "inconnu";
 	else $eleve = $_POST["realname"];
 if ($_POST["Score"] == "") $score = "inconnu";
 	else { 
-	     $score = $_POST["Score"];
+			 $score = $_POST["Score"];
 		 $note = $score/5;
 		 }
 if ($_POST["recipient"] == "") $recipient = "inconnu";
@@ -40,21 +43,21 @@ $rsActivite = mysqli_query($conn_intranet, $query_rsActivite) or die(mysqli_erro
 $row_rsActivite = mysqli_fetch_assoc($rsActivite);
 $totalRows_rsActivite = mysqli_num_rows($rsActivite);
 
-if ($totalRows_rsActivite<>'0') 
-{
-$sql = sprintf("UPDATE stock_activite SET score='%s', debut='%s', fin='%s' WHERE eleve_ID = '%s' AND quiz_ID='%s'", $note,$debut,$fin,$colname_rsActivite,$colname2_rsActivite);
+if ($totalRows_rsActivite<>'0') {
+	if ($row_rsActivite['score'] < $note) {
+		$sql = sprintf("UPDATE stock_activite SET score='%s', debut='%s', fin='%s' WHERE eleve_ID = '%s' AND quiz_ID='%s'", $note,$debut,$fin,$colname_rsActivite,$colname2_rsActivite);
+		mysqli_query($conn_intranet, $sql) or die(mysqli_error($conn_intranet)); 
+	}
 }
-	else
-{
-$sql = sprintf("INSERT INTO stock_activite ( `ID_activite` , `eleve_ID` , `identifiant` , `nom_classe` , `quiz_ID` , `score` , `debut` , `fin` , `fait` ) VALUES('','%s','%s','%s','%s','%s','%s','%s','%s')",$eleve_ID,$identifiant,$nom_classe,$quiz_ID,$note,$debut,$fin,$fait); 
+else {
+	$sql = sprintf("INSERT INTO stock_activite ( `ID_activite` , `eleve_ID` , `identifiant` , `nom_classe` , `quiz_ID` , `score` , `debut` , `fin` , `fait` ) VALUES('','%s','%s','%s','%s','%s','%s','%s','%s')",$eleve_ID,$identifiant,$nom_classe,$quiz_ID,$note,$debut,$fin,$fait);
+	mysqli_query($conn_intranet, $sql) or die(mysqli_error($conn_intranet)); 
 }
-mysqli_query($conn_intranet, $sql) or die('Erreur SQL !'.$sql.mysqli_error()); 
+
 mysqli_close($conn_intranet);  
 }
-?>
-<?php
-if ($_SESSION['Sess_nom']<>'VISITEUR') 
-{
+
+if ($_SESSION['Sess_nom']<>'VISITEUR') {
 	echo '<script type="text/javascript" language="javascript">';
 	echo ' opener.location.href ="../accueil_eleve.php?matiere_ID='.$_SESSION['matiere_ID'].'&niveau_ID='.$_SESSION['niveau_ID'].'&theme_ID='.$_SESSION['theme_ID'].'&categorie_ID='.$_SESSION['categorie_ID'].'"';
 	echo '</script>';
@@ -69,24 +72,15 @@ else
 
 <HTML>
 <link href="../style_jaune.css" rel="stylesheet" type="text/css">
-
-
-
 <script language="JavaScript" type="text/JavaScript">
-
-
 window.onload=function(){
-    window.setTimeout(function(){
-        window.close()
-    }, 10);
+		window.setTimeout(function(){
+				window.close()
+		}, 10);
 }
-
 </script>
-
 <title>Enregistrement des notes</title><body>
-
-   <?php
-
+<?php
 if ($_SESSION['Sess_nom']<>'VISITEUR')
 {
 if ((isset($_SESSION['Sess_prenom'])) && (isset($_SESSION['Sess_classe']))){
@@ -105,12 +99,12 @@ echo "Note : ".$note." /20  ";
 ?>
 <hr>
 <table width="100%" border="0" cellspacing="10" cellpadding="0">
-  <tr> 
-    <td><input name="Submit" type="submit" onClick="ferme_popup()" value="Faire un autre exercice"></td>
-  </tr>
-  <tr> 
-    <td><input name="Submit2" type="submit" onClick="deconnecter()" value="Se d&eacute;connecter       "></td>
-  </tr>
+	<tr> 
+		<td><input name="Submit" type="submit" onClick="ferme_popup()" value="Faire un autre exercice"></td>
+	</tr>
+	<tr> 
+		<td><input name="Submit2" type="submit" onClick="deconnecter()" value="Se d&eacute;connecter"></td>
+	</tr>
 </table>
 
  
